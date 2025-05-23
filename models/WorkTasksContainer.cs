2e4966cbb0c8138e840358e313cafc7771a21b31
CaueAnjos <kawidosanjos@outlook.com>
2025-05-22 21:57:29 -0300

    solving path problem

--- models/WorkTasksContainer.cs
+++ models/WorkTasksContainer.cs
@@ -5,17 +5,18 @@ namespace ToWork.Models;
 class WorkTasksContainer
 {
     public List<WorkTask> WorkTasks { get; set; } = new List<WorkTask> { };
+    private string path = "workTasks.bin";
 
     public void Save()
     {
-        using FileStream fs = new("workTasks.json", FileMode.Create, FileAccess.Write);
+        using FileStream fs = new(path, FileMode.Create, FileAccess.Write);
         using BinaryWriter bw = new(fs);
         bw.Write(WorkTasks);
     }
 
     public void Load()
     {
-        using FileStream fs = new("workTasks.json", FileMode.Open, FileAccess.Read);
+        using FileStream fs = new(path, FileMode.Open, FileAccess.Read);
         using BinaryReader br = new(fs);
         WorkTasks = br.ReadWorkTasks().ToList();
     }
